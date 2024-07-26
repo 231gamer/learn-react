3d6ceac11792173997ccecf0d2118a801572cbf3
Foday Malike Kabah <fodaymalikekabah@gmail.com>
2024-07-26 23:19:27 +0000

    React Intro

--- lecture-0/index.js
+++ lecture-0/index.js
@@ -1,1 +1,11 @@
-ReactDOM.render(<h1>Hello, World!</h1>, document.getElementById('root'))
+const navbar = (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(navbar, document.getElementById('root'))
